3e507ae536074a0c03ca041246e1019fc6deed08
Yab1 <yabilisanu@gmail.com>
2023-02-23 21:00:58 +0300

    task_2 clone

---
+++ 0x01-react_intro/task_3/dashboard/src/utils.js
@@ -0,0 +1,10 @@
+function getFullYear() {
+  return new Date().getFullYear();
+}
+function getFooterCopy(isIndex) {
+  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+}
+export default function getLatestNotification() {
+  return { __html: '<strong>Urgent requirement</strong> - complete by EOD' };
+}
+export { getFullYear, getFooterCopy };
