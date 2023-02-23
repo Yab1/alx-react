4d7e6e3b7a01a0b21b93df1af65420135951a367
Yab1 <yabilisanu@gmail.com>
2023-02-23 18:58:17 +0300

    Modify the App

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -12,6 +12,11 @@ function App() {
       <hr />
       <body className="App-body">
         <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+        <button>OK</button>
       </body>
       <hr />
       <footer className="App-footer">
