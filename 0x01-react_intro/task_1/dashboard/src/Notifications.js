8e6cd5b4a1f0b6d7670367b7b6d42517774245ce
Yab1 <yabilisanu@gmail.com>
2023-02-23 18:29:57 +0300

    create a Notifications element

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Notifications.css';
+
+function Notification() {
+  return (
+    <div className="Notification">
+      <p>Here is the list of notifications</p>
+    </div>
+  );
+}
+
+export default Notification;
