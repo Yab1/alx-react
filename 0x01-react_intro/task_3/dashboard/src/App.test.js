f7fd50e3592749cb3a5a1493fab94f3ab00ad89f
Yab1 <yabilisanu@gmail.com>
2023-03-08 08:32:52 +0300

    Create React tests for App

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,6 +1,24 @@
-import { screen, render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('should renders APP component without crashing', () => {
-  render(<App />);
+describe(App, () => {
+  it('should renders APP component without crashing', () => {
+    const app = shallow(<App />);
+    expect(app).toBeDefined();
+  });
+
+  it('should verify that App renders a div with the class App-header', () => {
+    const app = shallow(<App />);
+    expect(app.find('.App-header')).toBeDefined();
+  });
+
+  it('should verify that App renders a div with the class App-body', () => {
+    const app = shallow(<App />);
+    expect(app.find('App-body')).toBeDefined();
+  });
+
+  it('should verify that App renders a div with the class App-footer', () => {
+    const app = shallow(<App />);
+    expect(app.find('App-footer')).toBeDefined();
+  });
 });
