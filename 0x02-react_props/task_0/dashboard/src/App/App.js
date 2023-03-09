755105a20f9b304996327510cf96db9f73dd56ec
Yab1 <yabilisanu@gmail.com>
2023-03-09 19:59:27 +0300

    Modify the shell

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -1,29 +1,22 @@
-import React from "react";
-import logo from "../assets/holberton-logo.jpg";
-import "./App.css";
-import { getFullYear, getFooterCopy } from "../utils/utils";
+import React from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header.js';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
+    <>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <hr />
+        <Login />
+        <hr />
+        <Footer />
       </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password"></input>
-          <button>OK</button>
-        </form>
-      </div>
-      <div className="App-footer">
-        Copyright {getFullYear()} - {getFooterCopy()}
-      </div>
-    </div>
+    </>
   );
 }
 
