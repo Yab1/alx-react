638b7e91516c8e31a0871f59191725bb964c2c35
Yab1 <yabilisanu@gmail.com>
2023-03-09 16:23:40 +0300

    Create a Header component

---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,15 @@
+import logo from './Holberton Logo.jpg';
+
+const Header = () => {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <hr />
+    </>
+  );
+};
+
+export default Header;
