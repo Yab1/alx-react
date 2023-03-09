43962b67896dd963dcbc78486bf5efe6de06458f
Yab1 <yabilisanu@gmail.com>
2023-03-09 20:07:21 +0300

    App test

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -1,7 +1,10 @@
-import App from "./App";
+import React from 'react';
+import App from './App';
+import { shallow } from 'enzyme';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe(App, () => {
+  it('should render without crashing', () => {
+    const app = shallow(<App />);
+    expect(app).toBeDefined();
+  });
 });
