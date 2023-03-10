c01f555047dae8b9db63bb7f943f3e1a215e2775
Yab1 <yabilisanu@gmail.com>
2023-03-10 19:34:01 +0300

    Modify Notifications.test.js

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,29 +1,49 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Notifications from "./Notifications";
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Notifications from './Notifications';
+// import NotificationItem from "./NotificationItem";
 
-describe("Notification component tests", () => {
-  it("renders Notification component without crashing", () => {
+describe('Notification component tests', () => {
+  it('renders Notification component without crashing', () => {
     const notification = shallow(<Notifications />);
 
     expect(notification).toBeDefined();
   });
 
-  it("renders ul", () => {
+  it('renders ul', () => {
     const notification = shallow(<Notifications />);
 
-    expect(notification.find("ul")).toBeDefined();
+    expect(notification.find('ul')).toBeDefined();
   });
 
-  it("renders three list items", () => {
+  it('should check that the component renders NotificationItem elements', () => {
     const notification = shallow(<Notifications />);
-
-    expect(notification.find("li")).toHaveLength(3);
+    expect(notification.exists('NotificationItem')).toBe(true);
   });
 
-  it("renders correct text", () => {
+  it('renders correct text', () => {
     const notification = shallow(<Notifications />);
 
-    expect(notification.find("p").text()).toBe("Here is the list of notifications");
+    expect(notification.find('p').text()).toBe(
+      'Here is the list of notifications'
+    );
+  });
+  it('should check NotificationItem element renders the right html', () => {
+    const notification = shallow(<Notifications />);
+    const notificationItem = notification.find('NotificationItem');
+    notificationItem.forEach((li, index) => {
+      index === 0 &&
+        expect(li.html()).toEqual(
+          '<li data-notification-type="default">New course available</li>'
+        );
+      index === 1 &&
+        expect(li.html()).toEqual(
+          '<li data-notification-type="urgent">New resume available</li>'
+        );
+      index === 2 &&
+        expect(li.html()).toEqual(
+          '<li data-urgent="true"><strong>Urgent requirement</strong> - complete by EOD</li>'
+        );
+    });
   });
 });
