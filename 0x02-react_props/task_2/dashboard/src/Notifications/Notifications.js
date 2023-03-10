d03bc9e7b6a050c4340baa3ccc7c5f33616ef864
Yab1 <yabilisanu@gmail.com>
2023-03-10 19:33:39 +0300

    Modify Notifications.js

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -1,23 +1,34 @@
-import React from "react";
-import "./Notifications.css";
-import closeIcon from "../assets/close-icon.png";
-import { getLatestNotification } from "../utils/utils";
+import React from 'react';
+import './Notifications.css';
+import closeIcon from '../assets/close-icon.png';
+import { getLatestNotification } from '../utils/utils';
+import NotificationsItem from './NotificationItem';
 
 function Notifications() {
   return (
     <div className="Notifications">
       <button
-        style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "15px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
+        style={{
+          color: '#3a3a3a',
+          fontWeight: 'bold',
+          background: 'none',
+          border: 'none',
+          fontSize: '15px',
+          position: 'absolute',
+          right: '2px',
+          top: '2px',
+          cursor: 'pointer',
+        }}
         aria-label="Close"
-        onClick={console.log("Close button has been clicked")}
+        onClick={console.log('Close button has been clicked')}
       >
         <img src={closeIcon} alt="closeIcon" width="10px" />
       </button>
       <p>Here is the list of notifications</p>
       <ul>
-        <li data="default">New course available</li>
-        <li data="urgent">New resume available</li>
-        <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        <NotificationsItem type="default" value="New course available" />
+        <NotificationsItem type="urgent" value="New resume available" />
+        <NotificationsItem html={getLatestNotification()} />
       </ul>
     </div>
   );
