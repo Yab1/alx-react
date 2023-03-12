a6e95c907f454fb6ff5ac078dfe9ff642759e045
Yab1 <yabilisanu@gmail.com>
2023-03-12 16:45:22 +0300

    Update the UI

--- 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -6,7 +6,10 @@ const NotificationItem = ({ type, html, value }) => {
     <>
       {type && value ? <li data-notification-type={type}>{value}</li> : null}
       {html ? (
-        <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
+        <li
+          data-notification-type="urgent"
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></li>
       ) : null}
     </>
   );
