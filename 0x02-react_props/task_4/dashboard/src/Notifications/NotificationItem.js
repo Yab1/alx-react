6c5e3269c29741ca21f8dd3316c6b2bc3ef08189
Yab1 <yabilisanu@gmail.com>
2023-03-12 13:49:31 +0300

    Modify the NotificationItem component

--- 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropsTypes from 'prop-types';
 
 const NotificationItem = ({ type, html, value }) => {
   return (
@@ -11,4 +12,15 @@ const NotificationItem = ({ type, html, value }) => {
   );
 };
 
+NotificationItem.propsTypes = {
+  html: PropsTypes.shape({
+    __html: PropsTypes.string,
+  }),
+  type: PropsTypes.string.isRequired,
+  value: PropsTypes.string,
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+};
 export default NotificationItem;
