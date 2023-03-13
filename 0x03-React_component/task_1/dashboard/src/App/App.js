c6abc3f02e91f6e4662703b7009b650463b4f55a
Yab1 <yabilisanu@gmail.com>
2023-03-13 21:47:42 +0300

    Add lifecycle to a component

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -9,17 +9,29 @@ import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
 class App extends Component {
+  listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
+  ];
+  listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: getLatestNotification() },
+  ];
   render() {
-    const listCourses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 },
-    ];
-    const listNotifications = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', html: getLatestNotification() },
-    ];
+    const { isLoggedIn, logOut } = this.props;
+    onkeydown = onkeyup = function (e) {
+      let map = {};
+      e = e || event;
+      map[e.keyCode] = e.type == 'keydown';
+      // 72
+      if ((map[17] || map[89]) === false) {
+        this.alert('Logging you out');
+        logOut();
+      }
+    };
+
     return (
       <>
         <Notifications listNotifications={this.listNotifications} />
@@ -27,7 +39,7 @@ class App extends Component {
           <Header />
           <hr />
           <>
-            {this.props.isLoggedIn ? (
+            {isLoggedIn ? (
               <CourseList listCourses={this.listCourses} />
             ) : (
               <Login />
@@ -43,10 +55,14 @@ class App extends Component {
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
 };
 
 App.defaultProps = {
   isLoggedIn: false,
+  logOut: () => {
+    return;
+  },
 };
 
 export default App;
