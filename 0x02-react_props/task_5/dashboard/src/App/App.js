7855f6469114a3146ec07530a4df23f13d184a02
Yab1 <yabilisanu@gmail.com>
2023-03-13 18:19:50 +0300

    Added two arrays

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -6,15 +6,28 @@ import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import PropTypes from 'prop-types';
 import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
 
 function App({ isLoggedIn }) {
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
+  ];
+
+  const listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: getLatestNotification() },
+  ];
+
   return (
     <>
-      <Notifications />
+      <Notifications listNotifications={listNotifications} />
       <div className="App">
         <Header />
         <hr />
-        <>{isLoggedIn ? <CourseList /> : <Login />}</>
+        <>{isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}</>
         <hr />
         <Footer />
       </div>
