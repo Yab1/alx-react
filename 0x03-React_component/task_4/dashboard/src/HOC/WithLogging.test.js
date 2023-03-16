43585e77aa93099ba9cd8484c3bb5e1238225596
Yab1 <yabilisanu@gmail.com>
2023-03-16 20:25:20 +0300

    Write a test for the HOC

--- 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.test.js
+++ 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.test.js
@@ -7,8 +7,8 @@ const TestComponent = () => <p>Test Component</p>;
 describe('WithLogging tests', () => {
   it('should call console.log on mount and dismount', () => {
     const spy = jest.spyOn(console, 'log').mockImplementation();
-    const withLogging = WithLogging(TestComponent);
-    const wrapper = shallow(<withLogging />);
+    const NewComponent = WithLogging(TestComponent);
+    const wrapper = shallow(<NewComponent />);
 
     expect(spy).toBeCalledTimes(1);
     wrapper.unmount();
@@ -18,14 +18,14 @@ describe('WithLogging tests', () => {
 
   it('should log out the right message on mount and on unmount', () => {
     const spy = jest.spyOn(console, 'log').mockImplementation();
-    const withLogging = WithLogging(TestComponent);
-    const wrapper = shallow(<withLogging />);
+    const NewComponent = WithLogging(TestComponent);
+    const wrapper = shallow(<NewComponent />);
 
     expect(spy).toBeCalledTimes(1);
     expect(spy).toBeCalledWith('Component TestComponent is mounted');
     wrapper.unmount();
     expect(spy).toHaveBeenCalledTimes(2);
-    expect(spy).toBeCalledWith('Component Test is going to unmount');
+    expect(spy).toBeCalledWith('Component TestComponent is going to unmount');
     spy.mockRestore();
   });
 });
