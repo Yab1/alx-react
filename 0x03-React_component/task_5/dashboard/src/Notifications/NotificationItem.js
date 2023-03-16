f94499a7185adaf505f1fc5bf8ee04b389636e85
Yab1 <yabilisanu@gmail.com>
2023-03-16 14:35:54 +0300

    Initialize project using task_4_

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropsTypes from 'prop-types';
+
+class NotificationItem extends Component {
+  render() {
+    const { type, html, value, markAsRead, id } = this.props;
+    return (
+      <>
+        {type && value ? (
+          <li onClick={() => markAsRead(id)} data-notification-type={type}>
+            {value}
+          </li>
+        ) : null}
+        {html ? (
+          <li
+            onClick={() => markAsRead(id)}
+            data-notification-type={type}
+            dangerouslySetInnerHTML={{ __html: html }}
+          ></li>
+        ) : null}
+      </>
+    );
+  }
+}
+
+NotificationItem.propsTypes = {
+  __html: PropsTypes.shape({ html: PropsTypes.string }),
+  type: PropsTypes.string.isRequired,
+  value: PropsTypes.string,
+  markAsRead: PropsTypes.func,
+  id: PropsTypes.number,
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  markAsRead: () => {
+    console.log('Return Nothing');
+  },
+  id: 0,
+};
+export default NotificationItem;
