4736e2a594d6cf62e4fdfa4ae52c2db7e69e567f
Yab1 <yabilisanu@gmail.com>
2023-03-16 14:45:42 +0300

    Make your own pure component

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -6,9 +6,18 @@ import PropTypes from 'prop-types';
 import NotificationItemShape from './NotificationItemShape';
 
 class Notifications extends Component {
+  constructor(props) {
+    super(props);
+
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+  shouldComponentUpdate(nextProps) {
+    return nextProps.length > this.props.listNotifications.length;
+  }
   markAsRead(id) {
     console.log(`Notification ${id} has been marked as read`);
   }
+
   render() {
     const { displayDrawer, listNotifications } = this.props;
     return (
