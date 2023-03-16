71bfe44fa12943ca42c666cedb9ce541d024f9ca
Yab1 <yabilisanu@gmail.com>
2023-03-16 14:05:17 +0300

    Specialization

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class bodySectionWithMargin extends Component {
+  render() {
+    return (
+      <div className="bodySectionWithMargin">
+        <BodySection {...this.props} />
+      </div>
+    );
+  }
+}
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+};
+
+export default bodySectionWithMargin;
