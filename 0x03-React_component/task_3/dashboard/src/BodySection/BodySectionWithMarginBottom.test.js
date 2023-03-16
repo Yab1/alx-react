9322fafd48b673a6901896e6528f2d4136525b6a
Yab1 <yabilisanu@gmail.com>
2023-03-16 14:17:53 +0300

    Test the new components

--- 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -8,8 +8,8 @@ describe('BodySectionWithMarginBottom tests', () => {
     const wrapper = shallow(<BodySectionWithMarginBottom title="test title" />);
 
     expect(wrapper.find(BodySection)).toHaveLength(1);
-    // expect(wrapper.find(BodySection).html()).toEqual(
-    //   '<div class="bodySection"><h2>test title</h2></div>'
-    // );
+    expect(wrapper.find(BodySection).html()).toEqual(
+      '<div class="bodySection"><h2>test title</h2></div>'
+    );
   });
 });
