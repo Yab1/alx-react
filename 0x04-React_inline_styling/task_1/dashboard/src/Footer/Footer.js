02297283bb8bde563d7f1bd5e607922b75002799
Yab1 <yabilisanu@gmail.com>
2023-03-18 20:11:14 +0300

    Restore

--- 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import { getFullYear, getFooterCopy } from '../utils/utils';
-import { StyleSheet, css } from 'aphrodite';
 
 const Footer = () => {
   return (
-    <footer className={css(styles.AppFooter)}>
+    <footer className="App-footer">
       <p>
         Copyright {getFullYear()} - {getFooterCopy()}
       </p>
     </footer>
   );
 };
-
-const styles = StyleSheet.create({
-  AppFooter: {
-    fontSize: '0.8rem',
-    textAlign: 'center',
-    fontStyle: 'italic',
-    marginTop: '1em',
-  },
-});
 
 export default Footer;
