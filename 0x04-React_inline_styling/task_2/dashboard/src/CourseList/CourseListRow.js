c4ca4b9a1e89a435ae0534339aabe3539c3cb1b4
Yab1 <yabilisanu@gmail.com>
2023-03-19 19:25:09 +0300

    Modify the CourseListRow component

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
-  const rowStyle = { backgroundColor: '#f5f5f5ab' };
-  const headerStyle = { backgroundColor: '#deb5b545' };
   return (
-    <tr style={rowStyle}>
+    <tr className={isHeader ? css(styles.header) : css(styles.normal)}>
       {isHeader ? (
         !textSecondCell ? (
-          <th colSpan={2} style={headerStyle}>
+          <th colSpan={2} className={css(styles.th, styles.thColSpan)}>
             {textFirstCell}
           </th>
         ) : (
           <>
-            <th style={headerStyle}>{textFirstCell}</th>
-            <th style={headerStyle}>{textSecondCell}</th>
+            <th className={css(styles.th)}>{textFirstCell}</th>
+            <th className={css(styles.th)}>{textSecondCell}</th>
           </>
         )
       ) : (
         <>
-          <td>{textFirstCell}</td>
-          <td>{textSecondCell}</td>
+          <td className={css(styles.td)}>{textFirstCell}</td>
+          <td className={css(styles.td)}>{textSecondCell}</td>
         </>
       )}
     </tr>
@@ -37,5 +36,25 @@ CourseListRow.defaultProps = {
   isHeader: false,
   textSecondCell: null,
 };
+
+const styles = StyleSheet.create({
+  th: {
+    textAlign: 'start',
+    padding: '0.5em 0',
+    borderBottom: '1px solid rgb(180, 178, 178)',
+  },
+  thColSpan: {
+    textAlign: 'center',
+  },
+  normal: {
+    backgroundColor: '#f5f5f5ab',
+  },
+  header: {
+    backgroundColor: '#deb5b545',
+  },
+  td: {
+    padding: '.2em',
+  },
+});
 
 export default CourseListRow;
