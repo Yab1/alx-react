57de7b0560630ef510907fe2121bbcaefe6f482d
Yab1 <yabilisanu@gmail.com>
2023-03-22 13:48:17 +0300

    Modify the test suite

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -1,7 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Footer from './Footer';
 import { getFullYear, getFooterCopy } from '../utils/utils';
+import { AppContext } from '../App/AppContext';
 
 describe(Footer, () => {
   it('should renders without crashing', () => {
@@ -14,5 +18,36 @@ describe(Footer, () => {
     expect(footer.text()).toEqual(
       `Copyright ${getFullYear()} - ${getFooterCopy()}`
     );
+  });
+
+  it('should not display the link when the user is logged out', () => {
+    const context = {
+      user: {
+        isLoggedIn: false,
+      },
+    };
+    const footer = mount(
+      <AppContext.Provider value={context}>
+        <Footer />
+      </AppContext.Provider>
+    );
+
+    expect(footer.exists('a')).toBe(false);
+  });
+
+  it('should display the link when the user is logged in', () => {
+    const context = {
+      user: {
+        isLoggedIn: true,
+      },
+    };
+    const footer = mount(
+      <AppContext.Provider value={context}>
+        <Footer />
+      </AppContext.Provider>
+    );
+
+    expect(footer.exists('a')).toBe(true);
+    expect(footer.find('a').text()).toContain('Contact us');
   });
 });
