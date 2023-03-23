1a48c1eeed44014d48493742639001932dcb42ee
Yab1 <yabilisanu@gmail.com>
2023-03-23 21:11:57 +0300

    Filter a normalized Schema

--- 0x07. React Redux action creator+normalizr/task_2/dashboard/src/schema/notifications.js
+++ 0x07. React Redux action creator+normalizr/task_2/dashboard/src/schema/notifications.js
@@ -11,9 +11,17 @@ const notification = new schema.Entity('notifications', {
 const normalized = normalize(notificationData, [notification]);
 
 export default function getAllNotificationsByUser(userId) {
-  return notificationData
-    .filter((notification) => notification.author.id === userId)
-    .map((notification) => notification.context);
+  const output = [];
+  const notifications = normalized.entities.notifications;
+  const messages = normalized.entities.messages;
+
+  for (const id in notifications) {
+    if (notifications[id].author === userId) {
+      output.push(messages[notifications[id].context]);
+    }
+  }
+
+  return output;
 }
 
 export { normalized };
