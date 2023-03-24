e832131846a49368ed53053718b56c60408dc9ee
Yab1 <yabilisanu@gmail.com>
2023-03-24 10:26:49 +0300

    Create the action types

---
+++ 0x07. React Redux action creator+normalizr/task_3/dashboard/src/actions/courseActionTypes.js
@@ -0,0 +1,8 @@
+const SELECT_COURSE = {
+  type: 'Course/SELECT_COURSE',
+  payload: 'Define whether a user selected specific course',
+};
+const UNSELECT_COURSE = {
+  type: 'Course/UNSELECT_COURSE',
+  payload: 'Define whether a user selected unspecific course',
+};
