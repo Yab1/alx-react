10367faa1f88c928f79f54a699bb80afeae981cf
Yab1 <yabilisanu@gmail.com>
2023-03-25 14:38:38 +0300

    Initialize task using task_5

---
+++ 0x07. React Redux action creator+normalizr/task_6/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,141 @@
+import React, { PureComponent } from 'react';
+import closeIcon from '../assets/close-icon.png';
+import NotificationsItem from './NotificationItem';
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
+import { StyleSheet, css } from 'aphrodite';
+
+class Notifications extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {
+      displayDrawer,
+      listNotifications,
+      handleDisplayDrawer,
+      handleHideDrawer,
+      markNotificationAsRead,
+    } = this.props;
+
+    return (
+      <>
+        <div onClick={handleDisplayDrawer} className={css(styles.menuItem)}>
+          Your notifications
+        </div>
+        {displayDrawer ? (
+          <>
+            <div className={css(styles.Notifications)}>
+              <button
+                style={{
+                  color: '#3a3a3a',
+                  fontWeight: 'bold',
+                  background: 'none',
+                  border: 'none',
+                  fontSize: '15px',
+                  position: 'absolute',
+                  right: '2px',
+                  top: '2px',
+                  cursor: 'pointer',
+                }}
+                aria-label="Close"
+                onClick={handleHideDrawer}
+              >
+                <img src={closeIcon} alt="closeIcon" width="10px" />
+              </button>
+              {listNotifications.length !== 0 ? (
+                <p>Here is the list of notifications</p>
+              ) : null}
+              <ul>
+                {listNotifications && listNotifications.length > 0 ? (
+                  <>
+                    {listNotifications.map(({ id, html, type, value }) => (
+                      <NotificationsItem
+                        key={id}
+                        type={type}
+                        value={value}
+                        html={html}
+                        markAsRead={() => markNotificationAsRead(id)}
+                      />
+                    ))}
+                  </>
+                ) : (
+                  <NotificationsItem value="No new notification for now" />
+                )}
+              </ul>
+            </div>
+          </>
+        ) : null}
+      </>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  handleDisplayDrawer: PropTypes.func,
+  handleHideDrawer: PropTypes.func,
+  markNotificationAsRead: PropTypes.func,
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {},
+  markNotificationAsRead: () => {},
+};
+
+const opacityAnim = {
+  '0%': { opacity: 0.5 },
+  '100%': { opacity: 1 },
+};
+
+const bounceAnim = {
+  '0%': { transform: 'translateY(0px)' },
+  '33%': { transform: 'translateY(-5px)' },
+  '66%': { transform: 'translateY(5px)' },
+  '100%': { transform: 'translateY(0px)' },
+};
+
+const styles = StyleSheet.create({
+  Notifications: {
+    backgroundColor: 'white',
+    padding: '1em',
+    border: '2px dashed red',
+    position: 'absolute',
+    top: '40px',
+    right: '1.9em',
+    width: '30vw',
+    zIndex: '10',
+    '@media(max-width:900px)': {
+      inset: '0em',
+      width: '99.5vw',
+      fontSize: '20px',
+      padding: '0px',
+      ':nth-child(1n)>:last-child': {
+        padding: '0px',
+        listStyleType: 'none',
+      },
+      ':nth-child(1n)>:last-child > li': {
+        borderBottom: '0.1em solid black',
+        padding: '10px 8px',
+      },
+    },
+  },
+  menuItem: {
+    fontSize: '0.8rem',
+    textAlign: 'end',
+    paddingRight: '1.2em',
+    ':hover': {
+      cursor: 'pointer',
+      animationName: [opacityAnim, bounceAnim],
+      animationDuration: '1s, 0.5s',
+      animationIterationCount: '3',
+    },
+  },
+});
+
+export default Notifications;
