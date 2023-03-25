f9f2d2799b8f378559f7cb5242d743780e7af475
Yab1 <yabilisanu@gmail.com>
2023-03-25 14:28:08 +0300

    Create the action creator

--- 0x07. React Redux action creator+normalizr/task_5/dashboard/src/actions/notificationActionCreators.js
+++ 0x07. React Redux action creator+normalizr/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -1,8 +1,4 @@
-import {
-  MARK_AS_READ,
-  SET_TYPE_FILTER,
-  NotificationTypeFilters,
-} from './notificationActionTypes';
+import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
 
 export function markAsAread(index) {
   return {
