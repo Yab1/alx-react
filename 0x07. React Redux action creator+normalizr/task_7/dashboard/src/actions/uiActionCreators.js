9c80fa58fd8149a828bcc1be592b0ea1e3b23fc9
Yab1 <yabilisanu@gmail.com>
2023-03-25 18:37:11 +0300

    Async Action Creators

--- 0x07. React Redux action creator+normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x07. React Redux action creator+normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -3,7 +3,10 @@ import {
   LOGOUT,
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
 } from './uiActionTypes';
+import fetch from 'node-fetch';
 
 export function login(email, password) {
   return {
@@ -38,3 +41,25 @@ export function hideNotificationDrawer() {
 }
 export const boundHideNotificationDrawer = () =>
   dispatch(hideNotificationDrawer());
+
+export function loginSuccess() {
+  return {
+    type: LOGIN_SUCCESS,
+  };
+}
+
+export function loginFailure() {
+  return {
+    type: LOGIN_FAILURE,
+  };
+}
+
+export function loginRequest(email, password) {
+  return (dispatch) => {
+    boundLogin(email, password);
+    return fetch('http://localhost:8564/login-success.json')
+      .then((res) => res.json())
+      .then((json) => dispatch(loginSuccess()))
+      .catch((error) => dispatch(loginFailure()));
+  };
+}
--- 0x07. React Redux action creator+normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x07. React Redux action creator+normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -10,6 +10,9 @@ import {
   displayNotificationDrawer,
   hideNotificationDrawer,
 } from './uiActionCreators';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import fetchMock from 'fetch-mock';
 
 describe('tests for action creators', () => {
   it('should return right action payload and type when login is called', () => {
@@ -35,4 +38,46 @@ describe('tests for action creators', () => {
       type: HIDE_NOTIFICATION_DRAWER,
     });
   });
+
+  // it('should pass LOGIN and LOGIN_SUCCESS to the store if API returns the right response', () => {
+  // 	fetchMock.get('http://localhost:8564/login-success.json', {
+  // 		first_name: 'Yeabsera',
+  // 		last_name: 'Lisanework',
+  // 		email: 'yeabsera.lisanework@alxafrica.nz',
+  // 		profile_picture: 'http://placehold.it/32x32',
+  // 	});
+
+  // 	const store = mockStore({});
+  // 	const email = 'yeabsera.lisanework@alxafrica.nz';
+  // 	const password = 'password';
+
+  // 	return store.dispatch(
+  // 		loginRequest(email, password).then(() => {
+  // 			const actions = store.getActions();
+  // 			console.log(actions);
+  // 			expect(actions[0]).toEqual(loginSuccess());
+  // 			expect(actions[1]).toEqual(login());
+  // 		})
+  // 	);
+  // 	fetchMock.restore();
+  // });
+
+  // it('should pass LOGIN and LOGIN_FAILURE if API query fails', () => {
+  // 	fetchMock.get('http://localhost:8564/login-success.json', 400);
+
+  // 	const store = mockStore({});
+  // 	const email = 'yeabsera.lisanework@alxafrica.nz';
+  // 	const password = 'password';
+
+  // 	return store.dispatch(
+  // 		loginRequest(email, password).then(() => {
+  // 			const actions = store.getActions();
+  // 			console.log(actions);
+  // 			expect(actions[0]).toEqual(loginFailure());
+  // 			expect(actions[1]).toEqual(login());
+  // 		})
+  // 	);
+
+  // 	fetchMock.restore();
+  // });
 });
