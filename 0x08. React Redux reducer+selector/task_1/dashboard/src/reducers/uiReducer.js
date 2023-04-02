c3c3936c756330988ce190b470719a1ea3b7acc2
Yab1 <yabilisanu@gmail.com>
2023-04-02 21:08:36 +0300

    Use Immutable for the UI Reducer

--- 0x08. React Redux reducer+selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x08. React Redux reducer+selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -5,25 +5,26 @@ import {
   LOGIN_FAILURE,
   LOGOUT,
 } from '../actions/uiActionTypes';
+import { Map } from 'immutable';
 
-const defaultState = {
+export const defaultState = Map({
   isNotificationDrawerVisible: false,
   isUserLoggedIn: false,
   user: {},
-};
+});
 
 export default function uiReducer(state = defaultState, action) {
   switch (action.type) {
     case DISPLAY_NOTIFICATION_DRAWER:
-      return { ...state, isNotificationDrawerVisible: true };
+      return defaultState.set('isNotificationDrawerVisible', true);
     case HIDE_NOTIFICATION_DRAWER:
-      return { ...state, isNotificationDrawerVisible: false };
+      return defaultState.set('isNotificationDrawerVisible', false);
     case LOGIN_SUCCESS:
-      return { ...state, isUserLoggedIn: true };
+      return defaultState.set('isUserLoggedIn', true);
     case LOGIN_FAILURE:
-      return { ...state, isUserLoggedIn: false };
+      return defaultState.set('isUserLoggedIn', false);
     case LOGOUT:
-      return { ...state, isUserLoggedIn: false };
+      return defaultState.set('isUserLoggedIn', false);
     default:
       return state;
   }
--- 0x08. React Redux reducer+selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x08. React Redux reducer+selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -1,27 +1,20 @@
-import uiReducer from './uiReducer';
-import { DISPLAY_NOTIFICATION_DRAWER } from '../actions/uiActionTypes';
-import { SELECT_COURSE } from '../actions/courseActionTypes';
+import uiReducer, { defaultState } from './uiReducer';
+import { selectCourse } from '../actions/courseActionCreators';
+import { displayNotificationDrawer } from '../actions/uiActionCreators';
 
-describe('tests for uiReducer', () => {
-  const defaultState = {
-    isNotificationDrawerVisible: false,
-    isUserLoggedIn: false,
-    user: {},
-  };
-
-  it('should return initial state when no action is passed', () => {
-    expect(uiReducer(defaultState, 'null')).toEqual(defaultState);
+describe('uiReducer', () => {
+  it('Verifies initial state returned when no action is passed to uiReducer', () => {
+    const myState = uiReducer(defaultState, '');
+    expect(myState.toJS()).toEqual(defaultState.toJS());
   });
 
-  it('should return initial state when wrong action is passed', () => {
-    expect(uiReducer(defaultState, { type: SELECT_COURSE })).toEqual(
-      defaultState
-    );
+  it('Verifies the state equals the initial state when the action SELECT_COURSE is passed', () => {
+    const myState = uiReducer(defaultState, selectCourse());
+    expect(myState.toJS()).toEqual(defaultState.toJS());
   });
 
-  it('should change state correctly when action is passed', () => {
-    expect(
-      uiReducer(defaultState, { type: DISPLAY_NOTIFICATION_DRAWER })
-    ).toEqual({ ...defaultState, isNotificationDrawerVisible: true });
+  it('Verifies property isNotificationDrawerVisible is true when DISPLAY_NOTIFICATION_DRAWER passed', () => {
+    const myState = uiReducer(defaultState, displayNotificationDrawer());
+    expect(myState.toJS().isNotificationDrawerVisible).toEqual(true);
   });
 });
