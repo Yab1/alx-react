0901f6d009e6947dc54b85f51f13a358d63da61c
Yab1 <yabilisanu@gmail.com>
2023-04-03 11:22:56 +0300

    In the course reducer function

--- 0x08. React Redux reducer+selector/task_4/dashboard/src/reducers/courseReducer.js
+++ 0x08. React Redux reducer+selector/task_4/dashboard/src/reducers/courseReducer.js
@@ -1,39 +1,39 @@
 import {
-  FETCH_COURSE_SUCCESS,
-  SELECT_COURSE,
-  UNSELECT_COURSE,
+	FETCH_COURSE_SUCCESS,
+	SELECT_COURSE,
+	UNSELECT_COURSE,
 } from '../actions/courseActionTypes';
+import { Map } from 'immutable';
+import { coursesNormalizer } from '../schema/courses';
 
-const defaultState = [];
+const initialState = Map([]);
 
-export function courseReducer(state = defaultState, action) {
-  switch (action.type) {
-    case FETCH_COURSE_SUCCESS:
-      return action.data.map((course) => ({
-        ...course,
-        isSelected: false,
-      }));
-    case SELECT_COURSE:
-      return state.map((course) => {
-        if (course.id === action.index) {
-          return {
-            ...course,
-            isSelected: true,
-          };
-        }
-        return { ...course };
-      });
-    case UNSELECT_COURSE:
-      return state.map((course) => {
-        if (course.id === action.index) {
-          return {
-            ...course,
-            isSelected: false,
-          };
-        }
-        return { ...course };
-      });
-    default:
-      return state;
-  }
+export default function courseReducer(state = initialState, action) {
+	state = Map(state);
+	switch (action.type) {
+		case FETCH_COURSE_SUCCESS: {
+			const data = action.data.map((item) => ({
+				id: item.id,
+				name: item.name,
+				isSelected: false,
+				credit: item.credit,
+			}));
+			const normalizedData = coursesNormalizer(data);
+			return state.merge(normalizedData);
+		}
+		case SELECT_COURSE: {
+			return state.setIn(
+				['entities', 'courses', action.index.toString(), 'isSelected'],
+				true
+			);
+		}
+		case UNSELECT_COURSE: {
+			return state.setIn(
+				['entities', 'courses', action.index.toString(), 'isSelected'],
+				false
+			);
+		}
+		default:
+			return state;
+	}
 }
