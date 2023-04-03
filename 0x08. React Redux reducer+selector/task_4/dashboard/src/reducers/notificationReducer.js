e31e44f12f03a28ddc64df4c1fcab8451b3f95e6
Yab1 <yabilisanu@gmail.com>
2023-04-03 11:21:57 +0300

    Update the notification reducer

--- 0x08. React Redux reducer+selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ 0x08. React Redux reducer+selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,40 +1,47 @@
 import {
-  FETCH_NOTIFICATIONS_SUCCESS,
-  MARK_AS_READ,
-  SET_TYPE_FILTER,
+	MARK_AS_READ,
+	SET_TYPE_FILTER,
+	NotificationTypeFilters,
+	FETCH_NOTIFICATIONS_SUCCESS,
 } from '../actions/notificationActionTypes';
+import { Map } from 'immutable';
+import { notificationsNormalizer } from '../schema/notifications';
 
-const initialState = {
-  notifications: [],
-  filter: 'DEFAULT',
-};
+const initialState = Map({
+	notifications: [],
+	filter: 'DEFAULT',
+});
 
-export function notificationReducer(state = initialState, action) {
-  switch (action.type) {
-    case FETCH_NOTIFICATIONS_SUCCESS:
-      return {
-        ...state,
-        notifications: action.data.map((notification) => ({
-          ...notification,
-          isRead: false,
-        })),
-      };
-    case MARK_AS_READ:
-      return {
-        ...state,
-        notifications: state.notifications.map((notification) => {
-          if (action.index === notification.id) {
-            return { ...notification, isRead: true };
-          }
-          return { ...notification };
-        }),
-      };
-    case SET_TYPE_FILTER:
-      return {
-        ...state,
-        filter: action.filter,
-      };
-    default:
-      return state;
-  }
+export default function notificationReducer(state = initialState, action) {
+	switch (action.type) {
+		case FETCH_NOTIFICATIONS_SUCCESS: {
+			const notificationData = action.data.map((item) => ({
+				id: item.id,
+				type: item.type,
+				value: item.value,
+				isRead: false,
+			}));
+			return state.merge({
+				filter: NotificationTypeFilters.DEFAULT,
+				notifications: notificationsNormalizer(notificationData),
+			});
+		}
+		case MARK_AS_READ: {
+			return Map(state).setIn(
+				[
+					'notifications',
+					'entities',
+					'notifications',
+					action.index.toString(),
+					'isRead',
+				],
+				true
+			);
+		}
+		case SET_TYPE_FILTER: {
+			return Map(state).set('filter', action.filter);
+		}
+		default:
+			return state;
+	}
 }
